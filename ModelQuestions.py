e1c96ce21d87f6d18f0c84497f2ee560ab5020fb
David Saah <dasorange.hope@gmail.com>
2024-08-08 17:38:38 +0000

    feat: decluter model questions

---
+++ ModelQuestions.py
@@ -0,0 +1,7 @@
+risk_tolerance_req = """
+Generate a {content_type} related to {category} for users with
+{risk_tolerance} investment risk tolerance. Make articles
+conversation-like and educative. Maximum 200 words per article. Ensure the
+responses are relevant. Do not include any information apart from what is
+requested. Provide a hint that is educative and would help the user understand
+the topic better."""
